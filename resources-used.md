a465edbeba1eecfbc6254b3a9726ac0960f08511
denisS <densproatem@example.com>
2025-01-25 10:03:03 -0400

    Adding navbar

---
+++ resources-used.md
@@ -0,0 +1,4 @@
+# Resources used
+
+# Navbar
+styles and html where created folloring the [youtube video](https://www.youtube.com/watch?v=8eFeIFKAKHw)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    TranslateModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'dkasa';
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('es');
+  }
 }
