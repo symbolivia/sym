aa03b9d931b0f92d1083b4f56758c1395e33710e
denisS <densproatem@example.com>
2025-04-26 10:57:49 -0400

    fixing the router link between the pages

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [TranslateModule],
+  imports: [TranslateModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
