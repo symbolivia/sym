f565f6abaf47cb1179ced462d4c9aa28d63d05a5
denisS <densproatem@example.com>
2025-04-26 11:26:43 -0400

    Fixing the relative path for the icons not been displayed

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-footer',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, TranslateModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
   downloadCatalog() {
     const link = document.createElement('a');
-    link.href = '../../assets/images/catalog.pdf'; 
+    link.href = 'assets/images/catalog.pdf';
     link.download = 'catalog.pdf';
     link.click();
   }
